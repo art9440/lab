5a0f5caed16c7b18e557c49a81a33fb509da54b4
art9440 <144871779+art9440@users.noreply.github.com>
2023-09-22 18:11:08 +0700

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int fmaxx(int *arr, int len, int maxx, int i){
+
+    if (arr[i] > maxx)
+        maxx = arr[i];
+
+    if (i == len - 1)
+        return maxx;
+
+    return fmaxx(arr, len, maxx, i + 1);
+}
+
+int main(){
+    int len, *arr, maxx = 0, res;
+    scanf("%d", &len);
+    arr = (int*)malloc(len * sizeof(int));
+
+    for (int i = 0; i < len; i++)
+        scanf("%d", arr + i);
+
+    res = fmaxx(arr, len, maxx, 0);
+    printf("%d", res);
+
+
+    return 0;
+}
