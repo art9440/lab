548eb786a7a1ca6ce91b7035427a55264aacab8a
art9440 <144871779+art9440@users.noreply.github.com>
2023-09-22 16:42:55 +0700

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int sumnum(int n, int sum){
+    if (n == 0)
+        return sum;
+    
+    sum += n % 10;
+    return sumnum(n / 10, sum);
+}
+
+
+int main(){
+    int n, res, sum = 0;
+    scanf("%d", &n);
+    res = sumnum(n, sum);
+    printf("%d", res);
+}
