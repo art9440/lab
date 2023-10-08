27f641928e7594bbf018ac4c670009c52abe720f
art9440 <144871779+art9440@users.noreply.github.com>
2023-10-08 22:58:27 +0700

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+
+void sort(char **words, int countword){
+    for (int i = 0; i < countword - 1; i++){
+        for (int j = 0; j < countword - 1; j++){
+            if (strlen(words[j]) > strlen(words[j + 1])){
+                char *temp = words[j];
+                words[j] = words[j + 1];
+                words[j + 1] = temp;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < countword; i ++)
+        printf("%s ", words[i]);
+
+}
+
+
+int main(){
+    char str[1000];
+    gets(str);
+    char* words[100];
+    char* word = strtok(str, " ");
+    int countword = 0;
+    while(word != NULL) {
+        words[countword] = word;
+        countword++;
+        word = strtok(NULL, " ");
+    }
+    
+    sort(words, countword);
+
+
+    return 0;
+}
